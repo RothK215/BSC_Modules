cf1050a513f5368dcf1947986bc3c8af72529517
Rothana <112769542+RothK215@users.noreply.github.com>
2023-08-03 19:14:53 -0400

    New Rep

---
+++ sql-challenge/QuickDBD-sql challenge.sql
@@ -0,0 +1,86 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/hbAsBI
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+DROP TABLE Departments
+DROP TABLE Dept_Managers
+DROP TABLE Dept_Employees
+DROP TABLE Employees
+DROP TABLE Salaries
+DROP TABLE Titles
+---------------------------------------
+CREATE TABLE Departments (
+    dept_no VARCHAR(50)   NOT NULL,
+    dept_name VARCHAR(50)   NOT NULL)
+);
+
+SELECT * FROM Departments
+---------------------------------------
+CREATE TABLE Dept_Managers (
+    dept_no VARCHAR(50)   NOT NULL,
+    emp_no VARCHAR(50)   NOT NULL,
+    CONSTRAINT pk_Dept_Managers PRIMARY KEY (
+        emp_no
+     )
+);
+
+SELECT * FROM Dept_Managers
+---------------------------------------
+CREATE TABLE Dept_Employees (
+    emp_no VARCHAR(50)   NOT NULL,
+    dept_no VARCHAR(50)   NOT NULL,
+    CONSTRAINT pk_Dept_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+SELECT * FROM Dept_Employees
+---------------------------------------
+CREATE TABLE Employees (
+    emp_no VARCHAR(50)   NOT NULL,
+    emp_title VARCHAR(50)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    sex VARCHAR(50)   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+SELECT * FROM Employees
+---------------------------------------
+
+CREATE TABLE Salaries (
+    emp_no VARCHAR(50)   NOT NULL,
+    salary MONEY   NOT NULL
+);
+
+SELECT * FROM Salaries
+---------------------------------------
+CREATE TABLE Titles (
+    title_id VARCHAR(50)   NOT NULL,
+    title" VARCHAR(50)   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        title_id
+     )
+);
+
+SELECT * FROM Titles
+---------------------------------------
+
+ALTER TABLE "Dept_Managers" ADD CONSTRAINT "fk_Dept_Managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Managers" ADD CONSTRAINT "fk_Dept_Managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Salaries" ("emp_no");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title" FOREIGN KEY("title")
+REFERENCES "Employees" ("emp_title");
+
---
+++ sqlalchemy-challenge/app.py
@@ -0,0 +1,114 @@
+import numpy as np
+import datetime
+import json
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def prcp():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= '2016-08-23').\
+        order_by(Measurement.date).all()
+    session.close()
+
+    rain_measurement = []
+    format_data = "%Y-%m-%d"
+
+    for date, prcp in results:
+        rain_info = {}
+        rain_info["date"] = datetime.datetime.strptime(date, format_data)
+        rain_info["prcp"] = prcp
+        rain_measurement.append(rain_info)
+    return jsonify(rain_measurement)
+
+@app.route("/api/v1.0/stations")
+def station():
+    session = Session(engine)
+    results = session.query(Station.station, Station.name)
+    session.close()
+
+    station_list = []
+
+    for station, name in results:
+        info = {}
+        info["station"] = station
+        info["name"] = name
+        station_list.append(info)
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.tobs).\
+            filter(Measurement.station >= 'USC00519281').\
+            filter(Measurement.date >= '2016-08-18').\
+            order_by(Measurement.date).all()
+    session.close()
+
+    tobs = []
+    popular = 'USC00519281'
+    for day, temp in results:
+        temp_info = {}
+        temp_info["station"] = popular
+        temp_info["date"] = day
+        temp_info["temp"] = temp
+        tobs.append(temp_info)     
+    return jsonify(tobs)
+
+@app.route("/api/v1.0/2016-08-23")
+# Start date: 2016-08-23 to End date: 2017-08-23
+def start():
+    session = Session(engine)
+    mn = func.min(Measurement.tobs)
+    mx = func.max(Measurement.tobs)
+    ag = func.avg(Measurement.tobs)
+    results = session.query(mn, mx, ag).\
+        filter(Measurement.date >= '2016-08-23').all()
+    session.close()
+
+    stats = []
+
+    for n, m, a in results:
+        info = {"Date Range": "2016-08-23 to End"}
+        info["min"] = n
+        info["max"] = m
+        info["avg"] = a
+        stats.append(info)
+    return jsonify(stats)
+#@app.route("/api/v1.0/<start>/<end>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
